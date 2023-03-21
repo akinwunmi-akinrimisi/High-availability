36df678276015b7a48a977ca5162c313e32d0b73
Akinwunmi Akinrimisi <78056848+akinwunmi-akinrimisi@users.noreply.github.com>
2023-03-21 19:53:40 +0100

    Update HA.sh
    replaced the mysql line

--- HA.sh
+++ HA.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 sudo su
 yum update -y
-yum install -y httpd mysql
-amazon-linux-extras install -y php7.2
-systemctl start httpd
-systemctl enable httpd
-echo "Hello World from $(hostname -f)" > /var/www/html/index.html 
-echo "Healthy" > /var/www/html/health.html
+yum install -y httpd.x86_64
+systemctl start httpd.service
+systemctl enable httpd.service
+echo "Hello World from $(hostname -f)" > /var/www/html/index.html
+
